05d43c50933bc5fc25ea4b55a2d842fdf17d402f
Roman de la Rosa <mldevops2@gmail.com>
2023-10-31 20:20:45 -0600

    ploting the data: profit and population

--- machine_learning_course/exercise1_linear_regression/main.py
+++ machine_learning_course/exercise1_linear_regression/main.py
@@ -2,7 +2,6 @@
 import numpy as np # Linear Algebra
 import matplotlib.pyplot as plt # Data Visualization
 
-
 # Load the Data
 data = np.loadtxt(r'machine_learning_course\exercise1_linear_regression\source_data\ex1data1.txt', delimiter = ',')
 X = data[:, 0][:, np.newaxis] # Population
@@ -11,3 +10,17 @@ y = data[:, 1][:, np.newaxis] # Profit
 
 print(X)
 print(y)
+
+'''
+In this part of this exercise, you will implement linear regression with one variable to predict profits for a food truck. Suppose you are the CEO of a restaurant franchise and are considering different cities for opening a new outlet. The chain already has trucks in various cities and you have data for profits and populations from the cities. You would like to use this data to help you select which city to expand to next. The file ex1data1.txt contains the dataset for our linear regression problem. The first column is the population of a city and the second column is the profit of a food truck in that city. A negative value for profit indicates a loss
+'''
+
+# PLOTING THE DATA
+def plotData(X, y):
+    plt.figure(figsize = (11, 7))
+    plt.plot(X[:, 1], y, 'rx', maraksize = 10)
+    plt.grid()
+    plt.ylabel('Profit in $100,000s')
+    plt.xlabel('Population of City in 10,000s')
+
+plotData(X, y)
